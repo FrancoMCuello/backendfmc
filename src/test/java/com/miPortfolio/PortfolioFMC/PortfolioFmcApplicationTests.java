e2c33b6ed358a02659f2aab6ca4f7e185ec858ba
FrancoMCuello <franco.m.cuello@gmail.com>
2022-08-30 10:19:51 -0300

    Primer Commit

---
+++ src/test/java/com/miPortfolio/PortfolioFMC/PortfolioFmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miPortfolio.PortfolioFMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioFmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
